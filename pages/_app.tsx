6a4e63b1556a13861da67e8edb9ff1c17835d18f
Zikirullo <zikirulloabdumannopov@gmail.com>
2026-09-14 15:46:01 +0900

    feat: SASS integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { light } from "../scss/MaterialTheme/index";
 import { useState } from "react";
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import "../scss/app.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   // @ts-ignore
